472f2b43851df36c17b99cd4877a2876ebf6a8b4
Domin-Dev <domin.gamedev@gmail.com>
2024-03-22 21:16:43 +0100

    0.0135

--- Assets/Scripts/Grid/GridVisualization.cs
+++ Assets/Scripts/Grid/GridVisualization.cs
@@ -3,7 +3,6 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.UIElements.ToolbarMenu;
 
 public class GridVisualization: MonoBehaviour
 {
