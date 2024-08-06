d7858072471c2fc35c6e35f5dbcd06bc1a8509bf
Domin-Dev <domin.gamedev@gmail.com>
2024-08-06 20:48:36 +0200

    0.065

--- Assets/Scripts/Grid/MapGenerator.cs
+++ Assets/Scripts/Grid/MapGenerator.cs
@@ -12,12 +12,17 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private int heightInChunks = 256;
     [Header("Map Generator Settings")]
     [SerializeField] private float scale = 20;
-    [SerializeField] private float offsetX = 100f;
-    [SerializeField] private float offsetY = 100f;
+    [SerializeField] private Vector2 offset;
+    [Header("Temperature Map Settings")]
+    [SerializeField] private float scaleTemp = 20;
+    [SerializeField] private Vector2 offsetTemp;
+    [Header("Rainfall Map Settings")]
+    [SerializeField] private float scaleRain = 20;
+    [SerializeField] private Vector2 offsetRain;
     [Header("Chunk Settings")]
     [SerializeField] private int chunkSize = 10;
 
-    private static readonly Vector2 offset = new Vector2(-1,-1); 
+    private static readonly Vector2 gridOffset = new Vector2(-1,-1); 
 
     private MapGeneratorSettings mapGeneratorSettings;
     GridVisualization gridVisualization;
@@ -37,10 +42,16 @@ public class MapGenerator : MonoBehaviour
         mapGeneratorSettings = gridVisualization.settings;
 
         var rand = new System.Random(seed);
-        offsetX = rand.Next(-100000, 100000);
-        offsetY = rand.Next(-100000, 100000);
+        offset.x = rand.Next(-100000, 100000);
+        offset.y = rand.Next(-100000, 100000);
 
-        var map = GenerateMap(0.25f, offset);
+        offsetTemp.x = rand.Next(-100000, 100000);
+        offsetTemp.y = rand.Next(-100000, 100000);
+
+        offsetRain.x = rand.Next(-100000, 100000);
+        offsetRain.y = rand.Next(-100000, 100000);
+
+        var map = GenerateMap(0.25f, gridOffset);
         gridVisualization.SetMap(map); 
     }
 
@@ -74,37 +85,65 @@ public class MapGenerator : MonoBehaviour
             {
                 for (int x = 0; x < chunkSize; x++)
                 {
-                    float value = Generate(x + (int)item.Value.ChunkGridPosition.x, y + (int)item.Value.ChunkGridPosition.y);
-                    if (value > 0.65)
-                    {
-                        SetValue(item.Value,x, y, 0);
-                    }
-                    else if (value > 0.3)
-                    {
-                        SetValue(item.Value,x, y, 1);
-                    }
-                    else
-                    {
-                        SetValue(item.Value, x, y, 2);
-                    }
-                    
-                    if(rand.Next(0,100) <= 5)
-                    {
-                        SetBuildingObject(item.Value, x, y, 300);
-                    }
+                    GenerateTempCell(item.Value, x, y,rand);
                 }
             }
         }
         return map;
     }
 
-    private float Generate(int x,int y)
+    private void GenerateCell(Chunk chunk, int x, int y, System.Random rand)
     {
-        float xf = ((float)x  + offsetX )/ chunkSize * scale;
-        float yf = ((float)y + offsetY) / chunkSize * scale;
+        float value = Generate(x + (int)chunk.ChunkGridPosition.x, y + (int)chunk.ChunkGridPosition.y, offset, scale);
+        if (value > 0.65)
+        {
+            SetValue(chunk, x, y, 0);
+        }
+        else if (value > 0.3)
+        {
+            SetValue(chunk, x, y, 1);
+        }
+        else
+        {
+            SetValue(chunk, x, y, 2);
+        }
+
+        if (rand.Next(0, 100) <= 5)
+        {
+            SetBuildingObject(chunk, x, y, 300);
+        }
+    }
+
+    private void GenerateTempCell(Chunk chunk, int x, int y, System.Random rand)
+    {
+        float value = Generate(x + (int)chunk.ChunkGridPosition.x, y + (int)chunk.ChunkGridPosition.y,offsetRain,scaleRain);
+        if (value >= 0.8f)
+        {
+            SetValue(chunk, x, y, 3);
+        }
+        else if (value >= 0.6f)
+        {
+            SetValue(chunk, x, y, 4);
+        }
+        else if (value >= 0.4f)
+        {
+            SetValue(chunk, x, y, 5);
+        }
+        else if (value >= 0.2f)
+        {
+            SetValue(chunk, x, y, 6);
+        }
+        else
+        {
+            SetValue(chunk, x, y, 7);
+        }
+    }
+
+    private float Generate(int x, int y, Vector2 offset, float scale)
+    {
+        float xf = ((float)x  + offset.x )/ chunkSize * scale;
+        float yf = ((float)y + offset.y) / chunkSize * scale;
         float value = Mathf.PerlinNoise(xf,yf);
         return value;
     }
-
-
 }
